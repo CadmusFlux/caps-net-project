38f84d3c80adac03fd8b315ee954e7fa5ea568a7
Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2023-03-06 21:58:46 +0800

    Add augmentation builder

--- experiment_runner/utils/builder.py
+++ experiment_runner/utils/builder.py
@@ -1,12 +1,19 @@
-from typing import Any, Dict, Optional, Union
+from typing import Any, Dict, List, Union
 
-import tensorflow as tf
-from utils.layer import *
+import albumentations as A
 import tensorflow_addons as tfa
 
+from utils.layer import *
 
-def generate_layer(
-        name: str, layer_type: str, hparams: Optional[Dict[str, Any]] = None
+CIFAR10 = dict(
+    size=(32, 32), mean=(0.4914, 0.4822, 0.4465), std=(0.2470, 0.2435, 0.2616), pad=8
+)
+
+MNIST = dict(size=(28, 28), mean=0.1307, std=0.3081, pad=4)
+
+
+def create_layer(
+    name: str, layer_type: str, hparams: Optional[Dict[str, Any]] = None
 ) -> tf.keras.layers.Layer:
     if layer_type == "input":
         return tf.keras.layers.Input(name=name, **hparams)
@@ -25,50 +32,86 @@ def generate_layer(
 
     if layer_type == "flatten":
         return tf.keras.layers.Flatten(name=name)
-    
-    if layer_type == "primarycaps":
-        return PrimaryCapsule(name=name,**hparams)
-    
-    if layer_type == "CapsuleDR":
-        return CapsuleDR(name=name,**hparams)
-    
-    if layer_type == "CapsuleSA":
-        return CapsuleSA(name=name,**hparams)
-    
-    if layer_type == "CapsuleLength":
-        return CapsuleSA(name=name,**hparams)
-    
-    if layer_type == "CapsuleMask":
-        return CapsuleSA(name=name,**hparams)
+
+    if layer_type == "primary_capsule":
+        return PrimaryCapsule(name=name, **hparams)
+
+    if layer_type == "capsule_dr":
+        return CapsuleDR(name=name, **hparams)
+
+    if layer_type == "capsule_sa":
+        return CapsuleSA(name=name, **hparams)
+
+    if layer_type == "capsule_length":
+        return CapsuleSA(name=name, **hparams)
+
+    if layer_type == "capsule_mask":
+        return CapsuleMask(name=name, **hparams)
 
 
 def create_optimizer(
-        lr,kwargs: Union[Dict[str, Any], str]
+    learning_rate: Union[float, tf.keras.optimizers.schedules.LearningRateSchedule],
+    kwargs: Union[Dict[str, Any], str],
 ) -> Union[str, tf.keras.optimizers.Optimizer]:
     if isinstance(kwargs, str):
         return kwargs
 
-    #kwargs["hparams"]["learning_rate"] = float(kwargs["hparams"]["learning_rate"])
     kwargs["hparams"]["weight_decay"] = float(kwargs["hparams"]["weight_decay"])
 
     if kwargs["kind"] == "sgd":
-        return tfa.optimizers.SGDW(learning_rate = lr,momentum=0.9,**kwargs["hparams"])
+        return tfa.optimizers.SGDW(
+            learning_rate=learning_rate, momentum=0.9, **kwargs["hparams"]
+        )
 
     if kwargs["kind"] == "adam":
-        return tfa.optimizers.AdamW(learning_rate = lr,**kwargs["hparams"])
+        return tfa.optimizers.AdamW(learning_rate=learning_rate, **kwargs["hparams"])
+
+    raise ValueError("Optimizer is not specificed correctly!")
 
 
-def create_loss_function(kwargs: Union[Dict[str, Any], str]) -> tf.keras.losses.Loss:
-    if isinstance(kwargs, str):
-        return kwargs
+def create_transform(dataset: str, transforms: Optional[List[str]] = None):
+    dataset = dataset.upper()
+    if dataset not in ["MNIST", "CIFAR10"]:
+        raise ValueError("Dataset must either be mnist or cifar10")
 
-    if kwargs["kind"] == "sparse_cross_entropy":
-        return tf.keras.losses.SparseCategoricalCrossentropy(**kwargs["hparams"])
+    if dataset == "CIFAR10":
+        aug_args = CIFAR10
+    else:
+        aug_args = MNIST
 
-    if kwargs["kind"] == "cross_entropy":
-        return tf.keras.losses.CategoricalCrossentropy(**kwargs["hparams"])
+    normalize = A.Normalize(aug_args["mean"], aug_args["std"])
 
-    if kwargs["kind"] == "binary_cross_entropy":
-        return tf.keras.losses.BinaryCrossentropy(**kwargs["hparams"])
+    if transforms is None:
+        return normalize
 
-    raise ValueError("Loss function is not specified correctly!")
+    transforms_fn = []
+    for transform in transforms:
+        transform = transform.lower()
+
+        if transform == "random_crop":
+            transforms_fn.append(
+                A.PadIfNeeded(
+                    aug_args["size"][0] + aug_args["pad"],
+                    aug_args["size"][1] + aug_args["pad"],
+                    border_mode=0,
+                )
+            )
+            transforms_fn.append(A.RandomCrop(aug_args["size"][0], aug_args["size"][1]))
+
+        if transform == "color_jitter":
+            transforms_fn.append(A.ColorJitter())
+
+        if transform == "color_jitter_grayscale":
+            transforms_fn.append(A.OneOf([A.ColorJitter(p=1.0), A.ToGray(p=1.0)]))
+
+        if transform == "horizontal_flip":
+            transforms_fn.append(A.HorizontalFlip())
+
+        if transform == "vertical_flip":
+            transforms_fn.append(A.VerticalFlip())
+
+        if transform == "cutout":
+            transforms_fn.append(A.CoarseDropout())
+
+    transforms_fn.append(normalize)
+    return A.Compose(transforms_fn)
