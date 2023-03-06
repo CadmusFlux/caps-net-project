60b1deba2cb60f576bd2874cf3fbc859d5a16728
Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2023-03-06 22:00:33 +0800

    Add pretrained model loader

--- experiment_runner/main.py
+++ experiment_runner/main.py
@@ -1,12 +1,13 @@
 import argparse
 import random
 
-import joblib
 import yaml
+from keras_cv import models
 
 from utils.builder import *
 from utils.dataloader import *
 from utils.dataset import *
+from utils.model import *
 
 parser = argparse.ArgumentParser(
     prog="CapsNet-Experiment", description="Run experiments"
@@ -41,6 +42,22 @@ parser.add_argument(
 )
 
 parser.add_argument(
+    "--sample-percentage",
+    metavar="P",
+    type=float,
+    help="Percentage of sampled training data",
+    default=1.0,
+)
+
+parser.add_argument(
+    "--sample-size",
+    metavar="N",
+    type=int,
+    help="Fixed number of sampled training data",
+    default=-1,
+)
+
+parser.add_argument(
     "--random-seed",
     metavar="SEED",
     type=int,
@@ -49,16 +66,18 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    '--mixed-precision',
-    type=bool,
-    action='store_true',
-    default=False
+    "--independent",
+    action="store_true",
+    help="Use independent holdout sampling to have disjoint split per trial",
+    default=False,
 )
 
-
-def preprocess_image(image: tf.Tensor):
-    return dict(image=image / 255.0)
-
+parser.add_argument(
+    "--mixed-precision",
+    action="store_true",
+    help="Use mixed precision training",
+    default=False,
+)
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -67,18 +86,41 @@ if __name__ == "__main__":
     np.random.seed(args.random_seed)
     random.seed(args.random_seed)
 
+    sample_size = args.sample_size
+    if sample_size < 0:
+        sample_size = args.sample_percentage
+
     if args.mixed_precision:
-        tf.keras.mixed_precision.set_global_policy('mixed_float16')
+        tf.keras.mixed_precision.set_global_policy("mixed_float16")
 
     with open(args.blueprint) as f:
         blueprint = yaml.safe_load(f)
 
+        dataset = blueprint["dataset"]
         model_desc = blueprint["model"]
         train_args = blueprint["training_arguments"]
 
+        if "augmentation" not in train_args:
+            train_args["augmentation"] = None
+
         basepath = pathlib.Path(f"{args.export_folder}/{args.experiment_name}")
         savepath = basepath
-        for i in range(1, args.num_trials + 1):
+
+        os.makedirs(basepath, exist_ok=True)
+
+        transform_train = create_transform(dataset, train_args["augmentation"])
+        transform_eval = create_transform(dataset)
+
+        if dataset.upper() == "CIFAR10":
+            dataset = CIFAR10("train", transform_train)
+            dataset_valid = CIFAR10("valid", transform_eval)
+        elif dataset.upper() == "MNIST":
+            dataset = MNIST("train", transform_train)
+            dataset_valid = MNIST("valid", transform_eval)
+
+        dataset = dataset.sample(sample_size, args.num_trials, args.independent)
+
+        for i, dataset_train in enumerate(dataset, 1):
             tf.keras.backend.clear_session()
             print(f"Trial {i}/{args.num_trials}")
             if args.num_trials > 1:
@@ -87,33 +129,79 @@ if __name__ == "__main__":
 
             model = tf.keras.Sequential(name=model_desc["name"])
 
-            for name, options in model_desc["layer"].items():
-                if not "hparams" in options:
-                    options["hparams"] = {}
-                model.add(
-                    generate_layer(name, options["layer_type"], options["hparams"])
+            if model_desc["kind"] in ["standard", "capsule_network"]:
+                pretrained_args = dict(
+                    include_rescaling=False,
+                    include_top=True,
+                    input_shape=dataset_valid.image_size,
+                    classes=dataset_valid.num_classes,
+                    classifier_activation="linear",
                 )
+                if model_desc["name"] == "resnet18":
+                    backbone = models.resnet_v2.ResNet18V2(**pretrained_args)
+                elif model_desc["name"] == "resnet34":
+                    backbone = models.resnet_v2.ResNet34V2(**pretrained_args)
+                elif model_desc["name"] == "resnet50":
+                    backbone = models.resnet_v2.ResNet50V2(**pretrained_args)
+                elif model_desc["name"] == "densenet121":
+                    backbone = models.densenet.DenseNet121(**pretrained_args)
+                elif model_desc["name"] == "densenet169":
+                    backbone = models.densenet.DenseNet169(**pretrained_args)
+                elif model_desc["name"] == "vit-tiny":
+                    backbone = models.vit.ViTTiny16(**pretrained_args)
+                elif model_desc["name"] == "vit-small":
+                    backbone = models.vit.ViTS16(**pretrained_args)
+                elif model_desc["name"] == "vit-base":
+                    backbone = models.vit.ViTB16(**pretrained_args)
+                else:
+                    backbone = tf.keras.Sequential(name=model_desc["name"])
+                    for name, options in model_desc["layer"].items():
+                        if "hparams" not in options:
+                            options["hparams"] = {}
+                        backbone.add(
+                            create_layer(
+                                name, options["layer_type"], options["hparams"]
+                            )
+                        )
+
+                if model_desc["kind"] == "standard":
+                    model = StandardModel(backbone)
+                else:
+                    model = CapsNet(backbone)
+
+            else:
+                encoder = tf.keras.Sequential(name=model_desc["encoder"]["name"])
+                decoder = tf.keras.Sequential(name=model_desc["decoder"]["name"])
+
+                for name, options in model_desc["encoder"]["layer"].items():
+                    if "hparams" not in options:
+                        options["hparams"] = {}
+                    encoder.add(
+                        create_layer(name, options["layer_type"], options["hparams"])
+                    )
+
+                for name, options in model_desc["decoder"]["layer"].items():
+                    if "hparams" not in options:
+                        options["hparams"] = {}
+                    decoder.add(
+                        create_layer(name, options["layer_type"], options["hparams"])
+                    )
+
+                model = CapsNetWithDecoder(encoder, decoder)
+
+            trainloader = DataLoader(dataset_train, train_args["batch_size"], True)
+            validloader = DataLoader(dataset_valid, train_args["batch_size"])
+
+            learning_rate = tf.keras.optimizers.schedules.CosineDecay(
+                train_args["batch_size"] * float(train_args["base_learning_rate"]) / 16,
+                len(trainloader) * train_args["epochs"],
+            )
+
+            optimizer = create_optimizer(learning_rate, train_args["optimizer"])
+
+            model.compile(optimizer)
 
             model.summary()
-
-            trainset = CIFAR10(transform=preprocess_image)
-            validset = CIFAR10(train=False, transform=preprocess_image)
-
-            trainloader = DataLoader(trainset, train_args["batch_size"], True)
-            validloader = DataLoader(validset, train_args["batch_size"])
-
-            lr1 = tf.keras.optimizers.schedules.CosineDecayRestarts(
-                    train_args["batch_size"] * 1e-3 / 16, len(trainloader) * 100)
-
-            optimizer = create_optimizer(lr1,train_args["optimizer"])
-            loss = create_loss_function(train_args["loss"])
-            metrics = train_args["metrics"]
-
-            model.compile(
-                optimizer=optimizer,
-                loss=loss,
-                metrics=metrics,
-            )
 
             history = model.fit(
                 trainloader,
@@ -121,10 +209,11 @@ if __name__ == "__main__":
                 epochs=train_args["epochs"],
                 callbacks=[
                     tf.keras.callbacks.ModelCheckpoint(
-                        savepath.joinpath("weights/{epoch:03d}-{val_loss:.3f}.hdf5")
+                        savepath.joinpath("weights.hdf5"),
+                        save_best_only=True,
+                        save_weights_only=True,
                     ),
+                    tf.keras.callbacks.TerminateOnNaN(),
                     tf.keras.callbacks.CSVLogger(savepath.joinpath("train.csv")),
                 ],
             )
-
-            joblib.dump(history.history, savepath.joinpath("train.pkl"))
