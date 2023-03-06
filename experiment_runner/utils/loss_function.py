06e330b4494ab6a5ba2dd4aed7dab1775bd21235
Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2023-03-06 17:26:11 +0800

    Reformat code

--- experiment_runner/utils/loss_function.py
+++ experiment_runner/utils/loss_function.py
@@ -5,17 +5,16 @@ import tensorflow as tf
 
 class Margin(tf.keras.losses.Loss):
     def __init__(
-            self,
-            pos_weight: Optional[float] = 0.9,
-            lambda_: Optional[float] = 0.5,
+            self, pos_weight: Optional[float] = 0.9, lambda_: Optional[float] = 0.5,
             reduction: Optional[tf.keras.losses.Reduction] = tf.keras.losses.Reduction.AUTO,
-            name: Optional[str] = None,
-    ) -> None:
+            name: Optional[str] = None) -> None:
         super().__init__(reduction, name)
         self.pos_weight = pos_weight
         self.lambda_ = lambda_
 
     def call(self, y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
+        y_true = tf.cast(y_true, y_pred.dtype)
+
         loss_pos = y_true * tf.nn.relu(self.pos_weight - y_pred)
         loss_neg = (1 - y_true) * tf.nn.relu(y_pred + self.pos_weight - 1)
 
