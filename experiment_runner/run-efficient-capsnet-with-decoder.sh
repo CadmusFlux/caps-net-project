a948bc736980f20c282333df7a5cf7aa6bcdb9c8
Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2023-03-17 17:53:51 +0800

    Add capsnet scripts

---
+++ experiment_runner/run-efficient-capsnet-with-decoder.sh
@@ -0,0 +1,29 @@
+python main.py \
+    --blueprint blueprint/efficient-capsnet-with-decoder-full-aug.yaml \
+    --experiment-name efficient-capsnet-with-decoder-full-aug-full-sample \
+    --num-trial 5 \
+    --mixed-precision
+
+python main.py \
+    --blueprint blueprint/efficient-capsnet-with-decoder-full-aug.yaml \
+    --experiment-name efficient-capsnet-with-decoder-full-aug-ten-percent-sample \
+    --num-trial 5 \
+    --mixed-precision \
+    --sample-percentage 0.1 \
+    --independent
+
+python main.py \
+  --blueprint blueprint/efficient-capsnet-with-decoder-full-aug.yaml \
+  --experiment-name efficient-capsnet-with-decoder-full-aug-five-percent-sample \
+  --num-trial 5 \
+  --mixed-precision \
+  --sample-percentage 0.05 \
+  --independent
+
+python main.py \
+  --blueprint blueprint/efficient-capsnet-with-decoder-full-aug.yaml \
+  --experiment-name baseline-full-aug-one-percent-sample \
+  --num-trial 5 \
+  --mixed-precision \
+  --sample-percentage 0.01 \
+  --independent
