1e990b6223eafc8090422c078e3c6c1f9c161dae
Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2023-03-18 16:55:40 +0800

    Update mixed precision

--- experiment_runner/run-efficient-capsnet-with-decoder.sh
+++ experiment_runner/run-efficient-capsnet-with-decoder.sh
@@ -1,14 +1,12 @@
 python main.py \
     --blueprint blueprint/efficient-capsnet-with-decoder-full-aug.yaml \
     --experiment-name efficient-capsnet-with-decoder-full-aug-full-sample \
-    --num-trial 5 \
-    --mixed-precision
+    --num-trial 5
 
 python main.py \
     --blueprint blueprint/efficient-capsnet-with-decoder-full-aug.yaml \
     --experiment-name efficient-capsnet-with-decoder-full-aug-ten-percent-sample \
     --num-trial 5 \
-    --mixed-precision \
     --sample-percentage 0.1 \
     --independent
 
@@ -16,7 +14,6 @@ python main.py \
   --blueprint blueprint/efficient-capsnet-with-decoder-full-aug.yaml \
   --experiment-name efficient-capsnet-with-decoder-full-aug-five-percent-sample \
   --num-trial 5 \
-  --mixed-precision \
   --sample-percentage 0.05 \
   --independent
 
@@ -24,6 +21,5 @@ python main.py \
   --blueprint blueprint/efficient-capsnet-with-decoder-full-aug.yaml \
   --experiment-name baseline-full-aug-one-percent-sample \
   --num-trial 5 \
-  --mixed-precision \
   --sample-percentage 0.01 \
-  --independentcd
+  --independent
